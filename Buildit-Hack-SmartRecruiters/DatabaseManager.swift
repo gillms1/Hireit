4a828c10bd736e6500d0737eca9eef5d1c7af067
Tiago Bencardino <tiagobencardino@gmail.com>
2017-05-19 13:20:44 +0100

    Adding GoogleService-Info.plist

---
+++ Buildit-Hack-SmartRecruiters/DatabaseManager.swift
@@ -0,0 +1,19 @@
+//
+//  DatabaseManager.swift
+//  Buildit-Hack-SmartRecruiters
+//
+//  Created by Tiago Bencardino on 19/05/2017.
+//  Copyright © 2017 Manprit Singh Gill. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class DatabaseManager: NSObject {
+
+//    override init () {
+//        var ref = Database.database().reference()
+//    }
+
+
+}

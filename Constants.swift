f5860ee19a1fae613e9e76004659f871d2405887
Sunny <gillms1@users.noreply.github.com>
2017-05-24 09:35:09 +0100

    Update Constants.swift

--- Constants.swift
+++ Constants.swift
@@ -11,9 +11,9 @@ import Foundation
 private let BASE_URL = "https://api.smartrecruiters.com/v1/postings/"
 private let STATUS_END_POINT = "&units=metric"
 
-let API_KEY = "9094f33d1caf428fa939c0c250f2ccd2"
+let API_KEY = "xxxxx"
 let STATUS_URL = "\(BASE_URL)\(STATUS_END_POINT)London, uk\(API_KEY)"
-let STATUS_URL2 = "https://api.smartrecruiters.com/v1/postings/9094f33d1caf428fa939c0c250f2ccd2/candidates/24feb056-fadc-42b4-90ad-3fd7357ea668/status"
+let STATUS_URL2 = "https://api.smartrecruiters.com/v1/postings/xxxxxxxx/candidates/24feb056-fadc-42b4-90ad-3fd7357ea668/status"
 let CANDIDATES_URL = "https://api.smartrecruiters.com/candidates"
 
 typealias DownloadComplete = () -> ()
